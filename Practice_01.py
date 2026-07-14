6fbcb48aab46e899b80ffe2098c2d452084cf430
Sreepathi Lalitendra <lalitendrasreepathi544@gmail.com>
2026-07-14 16:16:37 +0530

    Task is added Successfully

---
+++ Practice_01.py
@@ -0,0 +1,5 @@
+print("Hello, World!")
+print("My Name is Sreepathi lalitendra")
+print("My goal is to become a python Expect")
+print("10+20") # --> The python interpreter treating it as string bro because wrote in qoutes bro 
+print(10 + 20) # it is add two number bro because we used + operater bro and we no wrote in quotes bro 
---
+++ Practice_02.py
@@ -0,0 +1,10 @@
+name = "Sreepathi lalitendra"
+goal = "python Expert"
+print(name)
+print(goal)
+# task 
+age = 25
+print(age)
+# upadating the value 
+age = age + 5 
+print(age)
