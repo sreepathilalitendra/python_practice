52e848e088607450bdbfc794fb99159e37dbc4cc
Sreepathi Lalitendra <lalitendrasreepathi544@gmail.com>
2026-07-15 06:27:44 +0530

    Task is added Successfully

---
+++ operators.py
@@ -0,0 +1,46 @@
+# Arithmetic operators in python    
+# Addition operator
+a = 34 
+b = 23
+print(a + b) # 57 because we used + operator to add two numbers
+# Subtraction operator
+a = 56 
+b = 43
+print(a-b) # 13 because we used - operator to subtract two numbers
+# Multiplication operator
+a = 54
+b = 32
+print(a*b) # 1728 because we used * operator to multiply two numbers
+# Division operator
+a = 10
+b = 5 
+print(a/b) # 2.0 because we used / operator to divide two numbers
+print(a//b) # 2 because we used // operator to divide two numbers and return the integer value floor division
+# Modulus operator 
+number = 10
+result = number % 2
+print(result) # 0 because 10 is divisible by 2
+number = 11
+result = number % 2
+print(result) # 1 because 11 is not divisible by 2
+a = 58
+b = 28
+print(a ** b) # 58 raised to the power of 28
+# Assignment operators in python
+x = 10
+print("initial value of x is:",x)
+x += 5
+print("X after X += 5:",x)
+# comparison operators in python
+a = 10
+b = 20
+print(a == b) # False because 10 is not equal to 20
+print(a != b) # True because 10 is not equal to 20
+print(a > b) # False because 10 is not greater than 20
+print(a < b) # True because 10 is less than 20
+print(a >= b) # False because 10 is not greater than or equal to 20
+print(a <= b) # True because 10 is less than or equal to 20
+# Task 
+num = 15
+is_even = (num % 2 == 0)
+print("Is the number even?", is_even) # False because 15 is not divisible by 2
