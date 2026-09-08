2593820ab4b6504fc8c23b98e48fb681d931037b
lalitendrasreepathi29-blip <lalitendrasreepathi544@gmail.com>
2026-09-08 14:58:31 +0530

    Day 1: Learn Python print statements and practice

---
+++ Day_01.py/practice.py
@@ -0,0 +1,18 @@
+# Day 1 - Practice
+
+# Challenge 1
+print("Hello Python")
+
+# Challenge 2
+print("Sreepathi Lalitendra")
+
+# Challenge 3
+print("I am learning Python")
+print("Python is easy")
+print("I will become a Python developer")
+
+# Challenge 4
+print("Name:", "Sreepathi Lalitendra")
+print("Age:", 18)
+print("Course:", "Diploma CSE")
+print("Skill:", "Python")
---
+++ Day_01.py/print_statement.py
@@ -0,0 +1,21 @@
+# Print statement
+print("Hello World")
+
+# Example 2
+print("Hello bro!")
+
+# Printing numbers
+print(10)
+print(25)
+print(100)
+
+# Print multiple things
+print("My name is Sreepathi Lalitendra")
+print("I am learning Python")
+print("Python is awesome")
+
+# Comments in Python
+# This is my first Python program
+
+print("Hello World")
+print("Hello World")  # Printing Hello World
