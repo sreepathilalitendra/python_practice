60cf9b9cdb3bdc68423811a112bb3184187071cb
lalitendrasreepathi29-blip <lalitendrasreepathi544@gmail.com>
2026-09-12 07:10:22 +0530

    Day 5: Python assignment operators

---
+++ Day_05.py/assignment_operators.py
@@ -0,0 +1,67 @@
+# Day - 5 Assignment Operators
+
+
+# Asssignment Operators (=)
+x = 10 
+print(x)
+
+
+# Example - 2
+x = 20 
+print(x)
+
+
+# Add and Assign Operator (+=)
+x = 10
+x = x + 5 
+print(x)
+
+
+# Example - 2 
+x = 10 
+x += 5
+print(x)
+
+
+# Subtract and Assign Operator (-=)
+x = 10 
+x -= 3
+print(x)
+
+
+# Multiply and Assign Operator (*=)
+x = 10
+x *= 3
+print(x)
+
+
+# Exponential and Assign Operator (**=)
+x = 10
+x **= 2
+print(x)
+
+
+# division and Assign Operator (/=)
+x = 10
+x /=2
+print(x)
+
+
+# Floor Division and Assign Operator (//=)
+x = 10 
+x //= 3 
+print(x)
+
+
+# Modulus and Assign Operator (%=)
+x = 10 
+x %= 3
+print(x)
+
+
+# Real Example of Assignment Operators
+money = 100
+money -= 20
+print(money)
+money += 50
+print(money)
---
+++ Day_05.py/practice.py
@@ -0,0 +1,30 @@
+# challenge 1 
+x = 100
+x += 20
+x -= 30
+x *= 2 
+x /= 5
+print(x)
+
+
+# challenge 2
+score = 100
+score += 25
+score -= 10
+score *= 2
+print("Final Score:", score)
+
+
+# challenge 3
+amount = int(input("Enter the amount:"))
+amount += 500
+amount -= 100
+print("Final Amount:", amount)
+
+
+# challenge 4
+balance = 1000
+balance -= 250
+balance -= 150
+balance += 500
+print("Final Balance:", balance)
