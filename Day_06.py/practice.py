1221e6ffd9b2172e33a3985e386b0055a85e46b9
lalitendrasreepathi29-blip <lalitendrasreepathi544@gmail.com>
2026-09-13 20:01:03 +0530

    Day 6: Comparison operators practice

---
+++ Day_06.py/practice.py
@@ -0,0 +1,30 @@
+# Challenge 1
+a = 20
+b = 10
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+print(a == b)
+print(a != b)
+
+
+# Challenge 2
+age = int(input("Enter your age: "))
+print(age >= 18)
+
+
+# Challenge 3
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print(a > b)
+print(a < b)
+print(a == b)
+print(a != b)
+
+
+# Challenge 4
+marks = int(input("Enter your marks: "))
+print(marks >= 40)
+print(marks >= 75)
+print(marks >= 100)
