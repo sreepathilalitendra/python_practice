1798f1101ec349076e522fabc5bbf32ca0dbc93e
lalitendrasreepathi29-blip <lalitendrasreepathi544@gmail.com>
2026-09-17 18:50:39 +0530

    Day 10: While loop and practice

--- Day_10.py/while_loop.py
+++ Day_10.py/while_loop.py
@@ -1,5 +1,21 @@
-# basic loop
-i = 1 
+# Day 10 - While Loop
+
+# Basic while loop
+i = 1
 while i <= 5:
     print(i)
     i = i + 1
+
+
+# While loop with user input
+number = int(input("Enter a number: "))
+while number <= 10:
+    print(number)
+    number = number + 1
+
+
+# Reverse while loop
+i = 10
+while i >= 1:
+    print(i)
+    i = i - 1
